4abc3ee9830d7239263d5e9092154a4309fd35ca
Andrey Fedyashov <andrey.fedyashov@gmail.com>
2023-09-24 02:47:26 -0700

    diagnostic fix

--- ApiTest/start-use-stop.ps1
+++ ApiTest/start-use-stop.ps1
@@ -6,21 +6,26 @@ $job = dotnet run --project "$PSScriptRoot/../ApiServer/ApiServer.csproj" --laun
 Write-Host "Waiting for the Api Server to initialize"
 Start-Sleep -Second 5
 
-Write-Host "Invoking the test method directly"
-$result = Invoke-RestMethod -Uri http://localhost:5000/operations/sayhello
-$result | Write-Host -ForegroundColor Green
-
-Write-Host "Invoking the test via Cmdlet/SDK"
-Import-Module "$PSScriptRoot/../ApiCmdlet/bin/Debug/netstandard2.0/ApiCmdlet.dll"
-$result = Invoke-SayHello -BaseUrl http://localhost:5000/
-$result | Write-Host -ForegroundColor Green
-
-Write-Host "Server job information"
-$job | Get-Job | Format-List | Out-String | Write-Host
-
-Write-Host "Stopping the server job"
-$job | Stop-Job
-
-Write-Host "Server job output"
-Get-Content "$PSScriptRoot/server.log" | Write-Host
+try 
+{
+    Write-Host "Invoking the test method directly"
+    $result = Invoke-RestMethod -Uri http://localhost:5000/operations/sayhelloXYZ
+    $result | Write-Host -ForegroundColor Green
+    
+    Write-Host "Invoking the test via Cmdlet/SDK"
+    Import-Module "$PSScriptRoot/../ApiCmdlet/bin/Debug/netstandard2.0/ApiCmdlet.dll"
+    $result = Invoke-SayHello -BaseUrl http://localhost:5000/
+    $result | Write-Host -ForegroundColor Green    
+}
+finally
+{    
+    Write-Host "Server job information"
+    $job | Get-Job | Format-List | Out-String | Write-Host
+    
+    Write-Host "Stopping the server job"
+    $job | Stop-Job
+    
+    Write-Host "Server job output"
+    Get-Content "$PSScriptRoot/server.log" | Write-Host
+}
 
